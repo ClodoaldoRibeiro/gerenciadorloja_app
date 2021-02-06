b0ea4f17ebe740c433f89ceb209688fc90ec6ecc
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2021-02-05 23:33:47 -0300

    Ajustar mensagens

--- lib/ui/android/products/widgets/alert_dialog_delete.dart
+++ lib/ui/android/products/widgets/alert_dialog_delete.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciadorloja_app/ui/themes/app_colors.dart';
 
 class AlertDialogDelete extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text("Alerta"),
-      content: Text("Tem certeza que deseja excluir?"),
+      content: Text("Esse produto será removido da sua lista de itens e " +
+          "não ficará mais disponível para vendas."),
       actions: [
         FlatButton(
-          child: Text("Cancelar"),
+          child:
+              Text("Cancelar", style: TextStyle(color: AppColors.COR_PRIMARIA)),
           onPressed: () {
             Navigator.of(context).pop(false);
           },
         ),
         FlatButton(
-          child: Text("Continar"),
+          child: Text(
+            "Excluir",
+            style: TextStyle(color: AppColors.COR_PRIMARIA),
+          ),
           onPressed: () {
             Navigator.of(context).pop(true);
           },
